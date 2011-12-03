24dbaabc8e862825e228bb7fbe45e92276584d93
Funky <funkydevnull@gmail.com>
2011-12-04 00:53:57 +0100

    Iniital commit: base dirs and some files

---
+++ src/core/vslBackendEntry.java
@@ -0,0 +1,37 @@
+package vsl.core;
+
+public class vslBackendEntry implements Serializable {
+
+	/* the ID of this entry in the backend */
+	protected vslID id;
+ 	
+	protected Boolean isCreated;
+
+	protected Boolean isLoaded;
+
+	public vslID getID()
+	{
+		return id;
+	}
+
+	public Boolean isCreated()
+	{
+
+	}
+
+	public Boolean isLoaded()
+	{
+
+	}
+
+	public vslFuture create()
+	{
+
+	}
+
+	public vslFuture load()
+	{
+
+	}
+
+}
