5396764b64a8012278bea4c53819cded684b0984
Hedeer El-Showk <hedeer@netherrealm.net>
2011-12-08 18:20:19 -0500

    cleaning

---
+++ src/vsl/core/vslID.java
@@ -0,0 +1,17 @@
+package vsl.core;
+
+public class vslID {
+	private String id;
+	
+	public vslID() {
+		id = "";
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String s) {
+		this.id = s;
+	}
+}
