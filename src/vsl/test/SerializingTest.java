16d282be62dddbcf940130e66f5f33320181b409
Funky <funkydevnull@gmail.com>
2011-12-08 15:00:23 +0100

    Added a file to test serializing objects and some basic instructions on using it.

---
+++ src/vsl/test/SerializingTest.java
@@ -0,0 +1,103 @@
+package vsl.test;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+import java.util.HashMap;
+import java.util.Date;
+import java.util.Iterator;
+//import java.util.ClassNotFoundException;
+
+public class SerializingTest
+{
+
+	private static HashMap map;
+	private static Date lastUpdate;
+
+	private static String mapFile = "mymap";
+
+	public static void main(String[] args)
+	{
+		if (args.length > 0)
+		{
+			if (args[0].equals("read"))
+			{
+				try{
+					readMap();
+					Iterator iter = map.keySet().iterator();
+					while(iter.hasNext())
+					{
+						Object key = iter.next();
+						System.out.println("[" + key + "]-->[" 
+								+ map.get(key) + "]");
+					}
+				} catch (Exception e) {
+					System.err.println("Caught exception: " + e.toString());
+				}
+				System.exit(0);
+				
+			}
+			if (args[0].equals("put") && args.length > 2)
+			{
+				System.out.println("Reading hashtable from file: " + mapFile);
+				try{
+					readMap();
+				} catch (FileNotFoundException e)
+				{
+					System.out.println("File not found, will create new file.");
+					map = new HashMap();
+				}
+				catch (Exception e) {
+					System.err.println("Caught exception: " + e.toString());
+					System.exit(1);
+				}
+				System.out.println("Putting [" + args[1] 
+						+ "]-->[" + args[2] +"]");
+				map.put(args[1], args[2]);
+				try{
+					writeMap();
+				} catch (IOException e) {
+					System.err.println("Caught exception: " + e.toString());
+				}
+			}
+			else
+			{
+				System.out.println("Must pass 'name' 'value' pair to put");
+			}
+		}
+		else
+		{
+			System.out.println(
+					"Usage: \tSerializingTest put <key> <val> \n" +
+					"\t SerializingTest read"); 
+		}
+
+	}
+
+	public static void readMap()
+		throws IOException, ClassNotFoundException
+	{
+		FileInputStream fis = new FileInputStream(mapFile);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		lastUpdate = (Date) ois.readObject();
+		map = (HashMap) ois.readObject();
+
+		ois.close();
+	}
+
+	public static void writeMap()
+		throws IOException
+	{
+		FileOutputStream fos = new FileOutputStream(mapFile);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(new Date());
+		oos.writeObject(map);
+
+		oos.close();
+	}
+
+}
